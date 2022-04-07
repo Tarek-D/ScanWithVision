7a3e5b24b56863ac6c0d8f14916c4db82f266461
Tarek Noubli <tareknoubli@gmail.com>
2022-04-07 14:27:49 +0200

    File added : - Document struct

---
+++ ScanWithVision/Model/Document.swift
@@ -0,0 +1,14 @@
+//
+//  Document.swift
+//  ScanWithVision
+//
+//  Created by Tarek Noubli on 07/04/2022.
+//
+
+import Foundation
+
+struct Document {
+    let id = UUID()
+    let date = Date()
+    var title: String
+}
